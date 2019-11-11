02fe26feac9cf778f5e03143a20a53e17de646ee
Michael Neely <michael@michaelneely.net>
2019-11-10 23:41:46 -0500

    Added posts capability

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,7 +88,7 @@
                             <div class="col-md-4">
                                 <div class="list-group">
                                     <div class="list-group-item">
-                                        <a href="">Posts</a>
+                                        <a href="{{ route('posts.index') }}">Posts</a>
                                     </div>
                                     <div class="list-group-item">
                                             <a href="{{ route('categories.index') }}">Categories</a>
