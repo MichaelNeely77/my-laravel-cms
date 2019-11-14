48b0823dedddffd52866727b7f270b3816e2eefc
Michael Neely <michael@michaelneely.net>
2019-11-13 23:59:57 -0500

    Adding images

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,6 +8,21 @@
 <div class="card card-default">
     <div class="card-header">Posts</div>
     <div class="card-body">
+        <table class="table">
+            <thead>
+                <th>Image</th>
+                <th>Title</th>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                <tr>
+                    <td><img src="{{ $post->image }}" alt=""></td>
+                    <td>{{ $post->title }}</td>
+                </tr>
+                    
+                @endforeach
+            </tbody>
+        </table>
         
     </div>
 </div>
