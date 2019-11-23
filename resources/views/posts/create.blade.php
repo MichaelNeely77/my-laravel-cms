3d62f52d4b076fd91b10d3529a2178b2cdf84356
Michael Neely <michael@michaelneely.net>
2019-11-23 00:46:14 -0500

    Still baffled

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -51,7 +51,7 @@
                     @foreach ($categories as $category)
                 <option value="{{ $category->id }}"
                     @if(isset($post))
-                        @if ($category->id == $post->category_id)
+                        @if ($category->id === $post->category_id)
                         selected
                         @endif
                     @endif
