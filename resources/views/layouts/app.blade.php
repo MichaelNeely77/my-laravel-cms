d19afa4c2ba99253a1efc66d4764d7fcc745a309
Michael Neely <michael@michaelneely.net>
2019-11-28 23:28:35 -0500

    Added JS back into the3 footer. All JS functions work again.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -136,5 +136,8 @@
 
 @yield('scripts')
 <script src="{‌{ asset('js/app.js') }}" defer></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
 </body>
 </html>
