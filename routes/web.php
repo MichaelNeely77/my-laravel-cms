022e05ee99bd629b1985bdf22220d66bceb42e19
Michael Neely <michael@michaelneely.net>
2019-11-30 23:24:26 -0500

    Added My Profile to User name dropdown

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::middleware(['auth'])->group(function () {
 });
 
 Route::middleware(['auth', 'admin'])->group(function() {
+    Route::get('users/profile', 'UsersController@edit')->name('users.edit-profile');
     Route::get('users', 'UsersController@index')->name('users.index');
     Route::post('users/{user}/make-admin', 'UsersController@makeAdmin')->name('users.make-admin');
 });
