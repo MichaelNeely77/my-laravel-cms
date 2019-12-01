c27d4473fb6696a434df661162b948726b7fd793
Michael Neely <michael@michaelneely.net>
2019-12-01 01:02:16 -0500

    Added the edit profile form and function

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,12 +5,35 @@ namespace App\Http\Controllers;
 use App\User;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\Users\UpdateProfileRequest;
+
 
 class UsersController extends Controller
 {
     public function index() 
     {
         return view('users.index')->with('users', User::all());
+    }
+
+    public function edit () 
+    {
+        return view('users.edit')->with('user', auth()->user());
+    }
+
+    public function update(UpdateProfileRequest $request) 
+    {
+        $user = auth()->user();
+
+        $user->update([
+
+            'name' => $request->name,
+            'about' => $request->about
+        ]);
+
+        session()->flash('success', 'Your profile has been made admin successfully');
+
+        return redirect()->back();
+
     }
 
     public function makeAdmin(User $user) 
