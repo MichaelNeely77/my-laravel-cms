2e5a0444876476e3f122f024cacf2b2f45da8777
Michael Neely <michael@michaelneely.net>
2019-12-05 23:55:46 -0500

    Adding the category's links  to the sidebar

--- app/Http/Controllers/Blog/PostsController.php
+++ app/Http/Controllers/Blog/PostsController.php
@@ -6,11 +6,22 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Post;
+use App\Category;
 
 class PostsController extends Controller
 {
     public function show (Post $post) 
     {
         return view('blog.show')->with('post', $post);
+    }
+
+    public function category (Category $category) 
+    {
+        return view('blog.category')->with('category', $category)->with('posts', $category->posts()->sinmplePaginate(3));
+    }
+
+    public function tag (Tag $tag) 
+    {
+        return view('blog.tag')->with('tag', $tag)->with('posts', $tag->posts()->sinmplePaginate(3));
     }
 }
